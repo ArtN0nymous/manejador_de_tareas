feb6a641454ef5d8c6d173880b002700228e8a52
ArtN0nymous <jr616934@gmail.com>
2023-01-07 21:24:45 -0700

    eliminar duplicados listo

--- src/pages/AllActivities/AllActivities.tsx
+++ src/pages/AllActivities/AllActivities.tsx
@@ -1,10 +1,45 @@
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
-import React, { useState,useContext } from "react";
+import React, { useState,useContext, useEffect } from "react";
 import PokemonsContext from "../../data/pokemons-context";
 // estilos personalizados
 import classes from './allActivities.module.css';
 const AllPokemons: React.FC=()=>{
     const activitiesCtxt=useContext(PokemonsContext);
+    const [state,setState]=useState([{
+        id:'',
+        type:'',
+        imgUrl:'',
+        base_experience:'',
+        name:''
+    }]);
+    useEffect(()=>{
+        let data = activitiesCtxt.pokemons;
+        let unicos:any=[];
+        //recorre el arreglo para ver elemento por elemento
+        for(var i = 0; i < data.length; i++) {
+            //tomanos el elemento en el indice i
+            const pokemon = data[i];
+            //inicializamos un estado como no duplicado en este caso falso
+            let esDuplicado = false;
+            //recorremos el arreglo donde no habrá repetidos
+            for(var j = 0; j < unicos.length; j++) {
+                //comparamos si el elemento en el indice j del arreglo nuevo tiene un id igual
+                // al elemento de nuestro array principal, si su id es igual está repetido
+              if (unicos[j].id === pokemon.id) {
+                //en caso de ser un id igual, indicamos el estado como repetido
+                esDuplicado = true;
+                break;
+              }
+            }
+            //validamos que nuestra variable "esDuplicado" sea falsa e insertamos el
+            // elemento en nuestro nuevo arreglo si no es duplicado
+            if (!esDuplicado) {
+              unicos.push(pokemon);
+            }
+        }
+        console.log(unicos);
+        setState(unicos);
+    },[activitiesCtxt])
     return(
         <IonPage>
             <IonHeader>
@@ -18,8 +53,8 @@ const AllPokemons: React.FC=()=>{
             <IonContent>
                 <IonGrid>
                     {
-                        activitiesCtxt.pokemons.map(item=>(
-                            <IonRow key={item.id}>
+                        state.map(item=>(
+                            <IonRow key={Math.random()}>
                                 <IonCol className="ion-text-center">
                                     <IonCard>
                                         <img src={item.imgUrl} alt='pokemon'/>
